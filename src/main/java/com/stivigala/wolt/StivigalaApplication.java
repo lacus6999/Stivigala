5cb738e25337b05e14cd52a40ffae55025032e13
kamu.laszlo <kamu.laszlo@promera.hu>
2021-02-24 15:40:03 +0100

    Initial spring boot

---
+++ src/main/java/com/stivigala/wolt/StivigalaApplication.java
@@ -0,0 +1,13 @@
+package com.stivigala.wolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StivigalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StivigalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stivigala/wolt/StivigalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stivigala.wolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StivigalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
