5872656a1f1f1084868ec3d7c6ad93392a2bd32a
kamu.laszlo <kamu.laszlo@promera.hu>
2021-03-12 10:30:39 +0100

    Authenticate progress

--- src/main/java/com/stivigala/wolt/WoltRestController.java
+++ src/main/java/com/stivigala/wolt/WoltRestController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WoltRestController {
 
-    @GetMapping("/admin")
-    public String adminPage() {
-        return "<p>This is an admin site.</p>";
-    }
-
-    @GetMapping("/user")
-    public String userPage() {
-        return "<p>This is a user site.</p>";
-    }
+//    @GetMapping("/admin")
+//    public String adminPage() {
+//        return "<p>This is an admin site.</p>";
+//    }
+//
+//    @GetMapping("/user")
+//    public String userPage() {
+//        return "<p>This is a user site.</p>";
+//    }
 
 }
