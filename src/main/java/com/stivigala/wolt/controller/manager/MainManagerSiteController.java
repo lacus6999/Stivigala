d06655d415833be2b79b0034a0dd9cbdca30fd53
Nemeskó Gábor <gabinemesk56@gmail.com>
2021-03-29 12:32:28 +0200

    Manager site

--- src/main/java/com/stivigala/wolt/controller/manager/MainManagerSiteController.java
+++ src/main/java/com/stivigala/wolt/controller/manager/MainManagerSiteController.java
@@ -1,23 +1,26 @@
 package com.stivigala.wolt.controller.manager;
 
+import com.stivigala.wolt.dbo.user.WoltUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 public class MainManagerSiteController {
 
     private final ManagerService managerService;
+    private final WoltUserService woltUserService;
 
-    public MainManagerSiteController(ManagerService managerService) {
+    public MainManagerSiteController(ManagerService managerService, WoltUserService woltUserService) {
         this.managerService = managerService;
+        this.woltUserService = woltUserService;
     }
 
     @GetMapping("/manager/mainManagerSite")
     public String showMainManagerPage(Model model) throws Exception {
         model.addAttribute("restaurants", managerService.findAllRestaurants());
+        model.addAttribute("currentUsername", woltUserService.getCurrentAuthenticatedUser().getUsername());
         return "manager/mainManagerSite";
     }
 
